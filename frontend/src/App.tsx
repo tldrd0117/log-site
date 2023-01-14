6c7a1e46dd89d58fc830329f7276fce26b90a2b3
tldrd0117 <tldrd0117@gmail.com>
2023-01-14 17:15:30 +0900

    add mui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import React from "react";
+import ButtonUnstyled from '@mui/base/ButtonUnstyled';
+import Button from "./components/Button";
+
 const App: React.FC = () => {
     return (
-        <div>hello world</div>
+        <div>
+            <Button>Hello World</Button>
+        </div>
     )
 };
 
